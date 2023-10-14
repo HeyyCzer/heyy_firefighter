c42d8d24546cf8dc239d02fdbc0a9ae943816274
HeyyCzer <rafa.pereira.carmo@gmail.com>
2023-10-14 19:02:43 -0300

    🔧 (cenários): Adicionado primeiro preset de cenário

--- config/scenarios.lua
+++ config/scenarios.lua
@@ -1,13 +1,132 @@
 ScenariosConfig = {
+	    --[[
+		["CentralBank"] = {
+			fires = {
+				{ coords = vec3(x, y, z), scale = 1.0, difficultyMultiplier = 5.0 },
+			},
+			vehicles = {
+				{ model = "model", coords = vec4(x, y, z, h) },
+			},
+			peds = {
+				{ model = "model", coords = vec4(x, y, z, h), animation = { upper, dict, anim, loop } },
+			},
+		}
+	]]
+
     ["CentralBank"] = {
         fires = {
-            { coords = vec3(x, y, z), rot = vec3(rx, ry, rz), difficultyMultiplier = 5.0 },
-        },
-        vehicles = {
-			{ model = "model", coords = vec4(x, y, z, h) },
-        },
-		peds = {
-			{ model = "model", coords = vec4(x, y, z, h), animation = { upper, dict, anim, loop } },
-		},
-    }
+			{
+				difficultyMultiplier = 1,
+				scale = 2,
+				["x"] = 232.02481079101562,
+				["y"] = 215.34483337402344,
+				["z"] = 105.94973754882812
+			},
+			{
+				difficultyMultiplier = 1,
+				scale = 2.25,
+				["x"] = 235.5568084716797,
+				["y"] = 210.38987731933594,
+				["z"] = 108.21776580810547
+			},
+			{
+				difficultyMultiplier = 1,
+				scale = 2.5,
+				["x"] = 231.02645874023438,
+				["y"] = 221.25997924804688,
+				["z"] = 111.82795715332031
+			},
+			{
+				difficultyMultiplier = 1,
+				scale = 2,
+				["x"] = 230.7657012939453,
+				["y"] = 214.8112030029297,
+				["z"] = 115.8279037475586
+			},
+			{
+				difficultyMultiplier = 1,
+				scale = 2,
+				["x"] = 246.29751586914062,
+				["y"] = 206.09481811523438,
+				["z"] = 104.19886779785156
+			},
+			{
+				difficultyMultiplier = 1,
+				scale = 2,
+				["x"] = 240.671630859375,
+				["y"] = 207.7877655029297,
+				["z"] = 104.29531860351562
+			},
+			{
+				difficultyMultiplier = 1,
+				scale = 2,
+				["x"] = 259.4308776855469,
+				["y"] = 203.61471557617188,
+				["z"] = 105.4544448852539
+			},
+			{
+				difficultyMultiplier = 1,
+				scale = 1.5,
+				["x"] = 266.6182861328125,
+				["y"] = 215.77520751953125,
+				["z"] = 105.62088012695312
+			},
+			{
+				difficultyMultiplier = 1,
+				scale = 1,
+				["x"] = 265.873291015625,
+				["y"] = 213.19236755371094,
+				["z"] = 106.09588623046875
+			},
+			{
+				difficultyMultiplier = 1,
+				scale = 1,
+				["x"] = 264.9903564453125,
+				["y"] = 210.76663208007812,
+				["z"] = 106.15897369384766
+			},
+			{
+				difficultyMultiplier = 1,
+				scale = 2,
+				["x"] = 259.6228942871094,
+				["y"] = 214.18478393554688,
+				["z"] = 105.60736083984375
+			},
+			{
+				difficultyMultiplier = 1,
+				scale = 2,
+				["x"] = 239.23863220214844,
+				["y"] = 225.95440673828125,
+				["z"] = 105.64684295654297
+			},
+			{
+				difficultyMultiplier = 1,
+				scale = 2,
+				["x"] = 238.93045043945312,
+				["y"] = 218.65829467773438,
+				["z"] = 105.68951416015625
+			},
+			{
+				difficultyMultiplier = 1,
+				scale = 2,
+				["x"] = 244.73838806152344,
+				["y"] = 219.32432556152344,
+				["z"] = 106.7884292602539
+			},
+			{
+				difficultyMultiplier = 1,
+				scale = 2,
+				["x"] = 260.1782531738281,
+				["y"] = 218.2815399169922,
+				["z"] = 105.50863647460938
+			},
+			{
+				difficultyMultiplier = 1,
+				scale = 2,
+				["x"] = 254.26971435546875,
+				["y"] = 209.70867919921875,
+				["z"] = 105.97605895996094
+			}
+		}
+	}
 }
